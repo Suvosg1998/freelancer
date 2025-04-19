a92cd06851580cac2fb97a8b116c34f0cb903a5e
Suvosg1998 <subhajitg1234567@gmail.com>
2025-04-20 00:45:35 +0530

    fourth commit

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 const db = require('./config/db');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/jobs', require('./routes/job.routes'));
