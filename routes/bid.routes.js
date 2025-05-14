66193b73f6a12916471733f91fc556f01d0ef874
Suvosg1998 <subhajitg1234567@gmail.com>
2025-05-15 02:44:46 +0530

    bid

--- routes/bid.routes.js
+++ routes/bid.routes.js
@@ -5,7 +5,7 @@ const { authMiddleware, authorizeRoles } = require('../middleware/auth.middlewar
 
 router.post('/:jobId', authMiddleware, authorizeRoles('freelancer'), bidController.placeBid);
 router.get('/client', authMiddleware, authorizeRoles('client'), bidController.getBidsForClientJobs);
-router.get('/:jobId', authMiddleware, bidController.getBidsForJob);
+router.get('/:jobId', authMiddleware, authorizeRoles('client'), bidController.getBidsForJob);
 router.patch('/:bidId/accept', authMiddleware, authorizeRoles('client'), bidController.acceptBid);
 router.patch('/:bidId/reject', authMiddleware, authorizeRoles('client'), bidController.rejectBid);
 
