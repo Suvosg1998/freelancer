a5c9623f95872c859d19f10edc1dcf3d9c057c64
Suvosg1998 <subhajitg1234567@gmail.com>
2025-05-30 22:31:07 +0530

    cors

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin:'http://localhost:5173',
-    credentials:true
-}));
+app.use(cors());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/auth', require('./routes/auth.routes'));
